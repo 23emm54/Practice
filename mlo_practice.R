cf9196e3738d0f41f9c0544d1ec3399356dd0195
23emm54 <147667761+23emm54@users.noreply.github.com>
2024-03-13 12:10:52 -0700

    finish

--- mlo_practice.R
+++ mlo_practice.R
@@ -21,20 +21,11 @@ str(mlo_monthly_co2)
 
 yr_av <- data.frame(mlo_monthly_co2$year, mlo_monthly_co2$month, mlo_monthly_co2$CO2, mlo_monthly_co2$sta)
 
-yr_av <- rename(yr_av, "year" = "mlo_monthly_co2.year", "month" = "mlo_monthly_co2.month", "co2" = "mlo_monthly_co2.co2", "station" = "mlo_monthly_co2.sta")
+yr_av <- rename(yr_av, "year" = "mlo_monthly_co2.year", "month" = "mlo_monthly_co2.month", "co2" = "mlo_monthly_co2.CO2", "station" = "mlo_monthly_co2.sta")
 View(yr_av)
-as.factor(yr_av$month)
 str(yr_av)
 
 yr_av%>%
   mutate(month = as.factor(month)%>%
            fct_recode("Jan" = "1", "Feb" = "2", "Mar" = "3", "Apr" = "4", "May" = "5", "Jun" = "6", 
                       "Jul" = "7", "Aug" = "8", "Sep" = "9", "Oct" = "10", "Nov" = "11", "Dec" = "12"))
-
-
-
-
-
-
-
-
